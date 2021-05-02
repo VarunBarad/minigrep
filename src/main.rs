5b9e0ff5da25ac17eec18f224a0f9046f0e56802
Varun Barad <varun@varunbarad.com>
2021-05-02 19:45:05 +0530

    Add code to read the file passed in argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +9,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_name);
+
+    let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
